dd700b55f04e1299d4842bb3ae988d493bc55b16
idealization <sanghwaann@gmail.com>
2021-02-06 19:26:36 +0900

    complete starting and stopping screenshot

--- app/flask-screenshot.py
+++ app/flask-screenshot.py
@@ -1,94 +1,65 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 13,
-   "id": "artificial-insider",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# all the imports\n",
-    "from flask import Flask\n",
-    "from flask import render_template\n",
-    "import pyautogui\n",
-    "import sys"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 14,
-   "id": "attractive-affairs",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# create our little application :)\n",
-    "app = Flask(__name__)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 15,
-   "id": "shaped-copper",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "app.route('/')\n",
-    "def index():\n",
-    "    return render_template('index.html')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "id": "material-district",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route('/capture/')\n",
-    "def capture():\n",
-    "    myScreenshot = pyautogui.screenshot()\n",
-    "    myScreenshot.save(r'C:\\Users\\sangh\\OneDrive\\문서\\PeopleSpace\\test.png')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "becoming-memphis",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "if __name__ == \"__main__\":\n",
-    "    app.run()"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "aerial-place",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# import pandas as pd
+# import numpy as np
+# from fastai.core import *
+# from PIL import Image
+# from fastai.script import *
+# from fastai.vision import *
+# from fastai.callbacks import *
+# from fastai.distributed import *
+# from fastprogress import fastprogress
+# from torchvision.models import *
+
+from flask import Flask, request, make_response
+from flask import render_template
+import pyautogui
+from time import gmtime, strftime
+from threading import Event, Thread
+
+
+def call_repeatedly(interval, func, *args):
+    stopped = Event()
+    def loop():
+        while not stopped.wait(interval): # the first call is in `interval` secs
+            func(*args)
+    Thread(target=loop).start()
+    return stopped.set
+
+
+app = Flask(__name__)
+capture_thread_stop = None
+
+
+@app.route('/start/')
+def start():
+    global capture_thread_stop
+    if capture_thread_stop:
+        return "Already started"
+    capture_thread_stop = call_repeatedly(1, capture)
+    return "Started"
+
+
+@app.route('/stop/')
+def stop():
+    global capture_thread_stop
+    if capture_thread_stop:
+        capture_thread_stop()
+        capture_thread_stop = None
+        return "Stopped"
+    else:
+        return "Not running"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def capture():
+    myScreenshot = pyautogui.screenshot()
+    myScreenshot.save(fr'C:\Users\sangh\OneDrive\문서\PeopleSpace\{strftime("%Y%m%d%H%M%S", gmtime())}.png')
+
+
+if __name__ == "__main__":
+    app.run()
+
+# In[ ]:
