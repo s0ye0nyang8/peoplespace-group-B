87ecd94e6fab6c259a3403b35437cb3ade6bf2e1
idealization <sanghwaann@gmail.com>
2021-02-07 21:17:05 +0900

    upload screenshot image to firebase storage

--- app/flask-screenshot.py
+++ app/flask-screenshot.py
@@ -8,12 +8,26 @@
 # from fastai.distributed import *
 # from fastprogress import fastprogress
 # from torchvision.models import *
-
+import storage as storage
 from flask import Flask, request, make_response
 from flask import render_template
 import pyautogui
 from time import gmtime, strftime
 from threading import Event, Thread
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import initialize_app
+from firebase_admin import storage
+import sys, os
+import tempfile
+from uuid import uuid4
+
+PROJECT_ID = "peoplespace-test"
+
+cred = credentials.Certificate('peoplespace-test-firebase-adminsdk-dawk1-a7fad79476.json')
+initialize_app(cred, {
+    'storageBucket': f'{PROJECT_ID}.appspot.com'
+})
 
 
 def call_repeatedly(interval, func, *args):
@@ -56,10 +70,16 @@ def index():
 
 def capture():
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(fr'C:\Users\sangh\OneDrive\문서\PeopleSpace\{strftime("%Y%m%d%H%M%S", gmtime())}.png')
+    #filename = fr'C:\Users\sangh\PycharmProjects\flask\{strftime("%Y%m%d%H%M%S", gmtime())}.png'
+    filename = strftime("%Y%m%d%H%M%S", gmtime())+'.png'
+    myScreenshot.save(filename)
+    bucket = storage.bucket()
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(filename)
+    temp = tempfile.NamedTemporaryFile(delete=False)
 
 
 if __name__ == "__main__":
     app.run()
 
-# In[ ]:
+
